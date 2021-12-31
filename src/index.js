2b80461f9a9a24d358d072cbe6fef05c1288436e
Ian McFarland <ian@neo.com>
2021-12-30 16:43:19 -0800

    Add key caps.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ class Square extends React.Component {
     render() {
       return (
         <button className="square">
-          {this.props.value}
+            <div className="letters">{this.props.value.letters}</div>
+            <div>{this.props.value.key}</div>
+          
         </button>
       );
     }
@@ -24,24 +26,24 @@ class Square extends React.Component {
         <div>
           <div className="status">{status}</div>
           <div className="key-row">
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            {this.renderSquare(3)}
+            {this.renderSquare({key: 1, letters: ''})}
+            {this.renderSquare({key: 2, letters: 'A B C'})}
+            {this.renderSquare({key: 3, letters: 'D E F'})}
           </div>
           <div className="key-row">
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            {this.renderSquare(6)}
+            {this.renderSquare({key: 4, letters: 'G H I'})}
+            {this.renderSquare({key: 5, letters: 'J K L'})}
+            {this.renderSquare({key: 6, letters: 'M N O'})}
           </div>
           <div className="key-row">
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            {this.renderSquare(9)}
+            {this.renderSquare({key: 7, letters: 'P Q R S'})}
+            {this.renderSquare({key: 8, letters: 'T U V'})}
+            {this.renderSquare({key: 9, letters: 'W X Y Z'})}
           </div>
           <div className="key-row">
-            {this.renderSquare('*')}
-            {this.renderSquare(0)}
-            {this.renderSquare('#')}
+            {this.renderSquare({key: '*', letters: ''})}
+            {this.renderSquare({key: 0, letters: 'OPER'})}
+            {this.renderSquare({key: '#', letters: ''})}
           </div>
         </div>
       );
