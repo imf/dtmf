b82aea1d1369654bc43a88126bb352460bcf5f9f
Ian McFarland <ian@neo.com>
2021-12-30 16:50:23 -0800

    Add tones to keys (but we can't play them yet) and add the A, B, C, D keys.

--- src/index.js
+++ src/index.js
@@ -26,24 +26,28 @@ class Square extends React.Component {
         <div>
           <div className="status">{status}</div>
           <div className="key-row">
-            {this.renderSquare({key: 1, letters: ''})}
-            {this.renderSquare({key: 2, letters: 'A B C'})}
-            {this.renderSquare({key: 3, letters: 'D E F'})}
+            {this.renderSquare({key: 1, letters: '', tones: [697, 1209]})}
+            {this.renderSquare({key: 2, letters: 'A B C', tones: [697, 1336]})}
+            {this.renderSquare({key: 3, letters: 'D E F', tones: [697, 1477]})}
+            {this.renderSquare({key: 'A', letters: '', tones: [697, 1633]})}
           </div>
           <div className="key-row">
-            {this.renderSquare({key: 4, letters: 'G H I'})}
-            {this.renderSquare({key: 5, letters: 'J K L'})}
-            {this.renderSquare({key: 6, letters: 'M N O'})}
+            {this.renderSquare({key: 4, letters: 'G H I', tones: [770, 1209]})}
+            {this.renderSquare({key: 5, letters: 'J K L', tones: [770, 1336]})}
+            {this.renderSquare({key: 6, letters: 'M N O', tones: [770, 1477]})}
+            {this.renderSquare({key: 'B', letters: '', tones: [770, 1633]})}
           </div>
           <div className="key-row">
-            {this.renderSquare({key: 7, letters: 'P Q R S'})}
-            {this.renderSquare({key: 8, letters: 'T U V'})}
-            {this.renderSquare({key: 9, letters: 'W X Y Z'})}
+            {this.renderSquare({key: 7, letters: 'P Q R S', tones: [852, 1209]})}
+            {this.renderSquare({key: 8, letters: 'T U V', tones: [852, 1336]})}
+            {this.renderSquare({key: 9, letters: 'W X Y Z', tones: [852, 1477]})}
+            {this.renderSquare({key: 'C', letters: '', tones: [852, 1633]})}
           </div>
           <div className="key-row">
-            {this.renderSquare({key: '*', letters: ''})}
-            {this.renderSquare({key: 0, letters: 'OPER'})}
-            {this.renderSquare({key: '#', letters: ''})}
+            {this.renderSquare({key: '*', letters: '', tones: [941, 1209]})}
+            {this.renderSquare({key: 0, letters: 'OPER', tones: [941, 1336]})}
+            {this.renderSquare({key: '#', letters: '', tones: [941, 1477]})}
+            {this.renderSquare({key: 'D', letters: '', tones: [941, 1633]})}
           </div>
         </div>
       );
